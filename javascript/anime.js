b7146ce80c50dcc2649e2c1d2211a0add40ffba8
masaaki hayashi <hayasb5y@gmail.com>
2020-10-10 17:12:33 +0900

    anime-comp

--- javascript/anime.js
+++ javascript/anime.js
@@ -1,3 +1,9 @@
-.sky {
-    background: URL("./img/sky.png")
-}
+let downtown = document.querySelector(".downtown");
+let car = document.querySelector(".car");
+
+addEventListener("keypress",function(x){
+    if(x.which === 13){
+        downtown.classList.toggle("moving");
+        car.classList.toggle("suspention");
+    }
+})
